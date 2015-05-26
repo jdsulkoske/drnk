add94ca9cc39cff187357a13a2009003eb001503
jdsulkoske <jdsulkoske@bsu.edu>
2015-05-26 14:17:42 -0400

    Created new tint colors for tab bar for selected and unselected tabs. These are not the final colors, and I might try to add background tint to tab bar.

--- drnk/BarsViewController.swift
+++ drnk/BarsViewController.swift
@@ -20,6 +20,9 @@ class BarsViewController: UIViewController, UITableViewDelegate {
         
         self.setUpBar()
         
+
+        
+        
     }
     
     
@@ -33,11 +36,13 @@ class BarsViewController: UIViewController, UITableViewDelegate {
         var bar2 = BarsInformation(name: "Cleo's Burboun Bar and Shit", address: "1625 W University Ave, Muncie IN 47303")
         var bar3 = BarsInformation(name: "Pop's Lovely Bar", address: "10799 East County Road 750 North, Brownsburg, IN 46112")
         var bar4 = BarsInformation(name: "Jake Sulks Wet & Wild Pub", address: "140 Ulen Boulevard Muncie, IN 47303")
+        var bar5 = BarsInformation(name: "David's Bar", address: "1110 West Neely Avenue Muncie, IN 47303")
         
         arrayOfBars.append(bar1)
         arrayOfBars.append(bar2)
         arrayOfBars.append(bar3)
         arrayOfBars.append(bar4)
+        arrayOfBars.append(bar5)
         
     }
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
@@ -72,7 +77,6 @@ class BarsViewController: UIViewController, UITableViewDelegate {
     
     @IBAction func showBarInformation(sender: AnyObject) {
         performSegueWithIdentifier("showBarInformationSegue", sender: sender)    }
-    
     
     
     
