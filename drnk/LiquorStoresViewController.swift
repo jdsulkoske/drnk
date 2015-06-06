a396bb34ee940a4c1ab4baaa96717baa909a9108
jdsulkoske <jdsulkoske@bsu.edu>
2015-06-06 18:14:14 -0400

    Created an array of Liquor Stores.

--- drnk/LiquorStoresViewController.swift
+++ drnk/LiquorStoresViewController.swift
@@ -12,10 +12,14 @@ class LiquorStoresViewController: UIViewController {
 
     @IBOutlet weak var liquorStoreTableVIew: UITableView!
     
+    var arrayOfLiquorStores: [LiquorStoresInformation] = [LiquorStoresInformation]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view, typically from a nib.
+        self.setUpLiquorStore()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +27,18 @@ class LiquorStoresViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setUpLiquorStore(){
+        var ls1 = LiquorStoresInformation(lsName: "Muncie Liquors", lsAddress: "1110 West Neely Avenue", lsImage: "BeerGlass.png")
+        var ls2 = LiquorStoresInformation(lsName: "Friendly Package", lsAddress: "10213 Nicole Drive", lsImage: "Headphones.png")
+        var ls3 = LiquorStoresInformation(lsName: "Muncie Liquors", lsAddress: "909 West Riverside", lsImage: "redsolocupicon.png")
+        var ls4 = LiquorStoresInformation(lsName: "Another Liquor Store", lsAddress: "505 North EverWood Dr.", lsImage: "VCImage.png")
+        
+        arrayOfLiquorStores.append(ls1)
+        arrayOfLiquorStores.append(ls2)
+        arrayOfLiquorStores.append(ls3)
+        arrayOfLiquorStores.append(ls4)
+
+    }
 
 }
 
