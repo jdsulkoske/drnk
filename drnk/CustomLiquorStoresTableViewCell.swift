ffd10fb906d9cf09efba5b1d7730f2ccd3855f97
jdsulkoske <jdsulkoske@bsu.edu>
2015-06-06 17:52:20 -0400

    Created two group folders to keep liquor store and bars information filed

---
+++ drnk/CustomLiquorStoresTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomLiquorStoresTableViewCell.swift
+//  drnk
+//
+//  Created by Jake Sulkoske on 6/6/15.
+//  Copyright (c) 2015 Sulk. All rights reserved.
+//
+
+import UIKit
+
+class CustomLiquorStoresTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
