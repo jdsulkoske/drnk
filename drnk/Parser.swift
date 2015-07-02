b12b6fde1a3ecb43615c52d91ed97c830e7ac744
frshatat <farisshatat4@yahoo.com>
2015-07-02 15:47:25 -0500

    Added day function to parser to find the current special for the day

--- drnk/Parser.swift
+++ drnk/Parser.swift
@@ -13,33 +13,39 @@ class Parser{
     var barSpecialArray = [String]()
     var address = " "
     var name = ""
-    var results : NSDictionary!
+    
+    var dayOfTheWeek : Day = Day()
+    
+    var deals : NSDictionary!
     init(jsonFile:NSArray){
         self.jsonFile = jsonFile
+        let date = NSDate()
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "EEEE"
+        let day = formatter.stringFromDate(date)
+        dayOfTheWeek.findDay(day)
+        dayOfTheWeek.intValueToDayString(dayOfTheWeek.getIntValueOfDay())
+      
        
     }
     
     
     
     func parseBarInfo(){
-        
-      
         for posts in jsonFile {
             address = posts["company_street"] as! String
             name = posts["company_name"] as! String
-            results = (posts["deals"] as? NSDictionary)!
-            //println(results)
-        
+            deals = (posts["deals"] as? NSDictionary)!
+            self.parseForSpecial()
+
         }
-        self.parseForSpecial()
+        
     }
     func parseForSpecial(){
-     var days = results["friday"] as! NSArray
-        println(days)
+     var days = deals[dayOfTheWeek.getDayAsString().lowercaseString] as! NSArray
         for posts in days{
                 var barspecial = posts["deal_name"] as! String
                 barSpecialArray.append(barspecial)
-      
             }
          bar = BarsInformation(name: name, address: address,barImage:"VCImage.png",special1: barSpecialArray[0], special2:barSpecialArray[1],special3: barSpecialArray[2])
         arrayOfBars.append(bar)
