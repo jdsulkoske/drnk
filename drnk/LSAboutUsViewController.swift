0ffb689aa4cba385a3d6748c02d730a333488506
frshatat <farisshatat4@yahoo.com>
2015-07-14 12:54:16 -0500

    idk

---
+++ drnk/LSAboutUsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LSAboutUsViewController.swift
+//  Pods
+//
+//  Created by faris shatat on 7/13/15.
+//
+//
+
+import Foundation
+class LSAboutUSViewController: UIViewController{
+    
+    @IBOutlet weak var todaysHoursLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+}
