af6637d93536b24ee21158758cbcd6b60914c24f
jdsulkoske <jdsulkoske@bsu.edu>
2015-07-30 11:17:24 -0400

    Attempting to fix problem

--- drnk/TodayContainerViewController.swift
+++ drnk/TodayContainerViewController.swift
@@ -37,9 +37,35 @@ class TodayContainerViewController: UIViewController,UITableViewDelegate {
         
         data.getData { (responseObject, error) -> Void in
             if  responseObject == nil{
+                
+                
+                
+                self.data.getData { (responseObject, error) -> Void in
+                    
+                    if  responseObject != nil{
+                    
+                    let parser = Parser(jsonFile: responseObject!)
+                    todaysSpecialArray.removeAll(keepCapacity: true)
+                    
+                    dispatch_async(dispatch_get_main_queue()){
+                        
+                        parser.findTodaysSpecial()
+                        
+                        self.myTableView.reloadData()
+                        }
+                        println("fuck")
+                        
+                    } else {
+                     
+                        println("super fucked")
+                        
+                    }
+                    
+                }
 
             } else {
                 
+                println("not fucked")
                // self.networkMessage.hidden = true
                 let parser = Parser(jsonFile: responseObject!)
                 todaysSpecialArray.removeAll(keepCapacity: true)
