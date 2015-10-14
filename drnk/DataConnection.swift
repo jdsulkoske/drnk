718c41e89a417a88aa17514e9bf93b82e5221021
jdsulkoske <jdsulkoske@bsu.edu>
2015-10-14 12:08:55 -0400

    Pod update made the JSON request syntax change. Should be up to date now.

--- drnk/DataConnection.swift
+++ drnk/DataConnection.swift
@@ -31,19 +31,32 @@ class DataConnection {
 //            }
 //            completionHandler(responseObject: self.post , error: error)
 //        }
-        Alamofire.request(.GET, requestString)
-            .responseJSON { _, _, result in
-                switch result {
-                case .Success(let data):
+//        Alamofire.request(.GET, requestString)
+//            .responseJSON { _, _, result in
+//                switch result {
+//                case .Success(let data):
+//                    self.post = data as! NSArray
+//                    debugPrint(self.post[1])
+//                case .Failure(_, let error):
+//                    self.error = error
+//                    print("Request failed with error: \(error)")
+//                
+//                }
+//                completionHandler(responseObject: self.post , error: self.error)
+//
+//        }
+        Alamofire.request(.GET, requestString).responseJSON() {
+            response in
+            if response.result.isSuccess {
+                let data = response.result.value
                     self.post = data as! NSArray
                     debugPrint(self.post[1])
-                case .Failure(_, let error):
-                    self.error = error
-                    print("Request failed with error: \(error)")
-                
-                }
+                    
+            } else {
+                let error = self.error
+                print("Request failed with error: \(error)")
+            }
                 completionHandler(responseObject: self.post , error: self.error)
-
         }
         
     }
